2a6fae24bc7b58af4022ac3b7ee04bd52919f59c
Hampus <hampus.olsson88@gmail.com>
2020-03-11 22:22:57 +0100

    Solved day01 puzzle

---
+++ day01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileInput = File.ReadAllText("input.txt");
+
+        }
+
+    }
+}
