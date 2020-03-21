fc137b007b477eca3dd2b071831e81e0a3560218
Hampus <hampus.olsson88@gmail.com>
2020-03-21 14:37:51 +0100

    Added missing code

--- day01/Program.cs
+++ day01/Program.cs
@@ -10,7 +10,33 @@ namespace day01
         static void Main(string[] args)
         {
             string fileInput = File.ReadAllText("input.txt");
+            List<string> masses = fileInput.Split("\r\n").ToList();
+            int _fuel = 0;
+            int _extrafuel = 0;
 
+            foreach (var mass in masses)
+            {
+                int tmpmass = Convert.ToInt32(mass);
+                
+                tmpmass = fuel(tmpmass);
+                _fuel += tmpmass;
+
+                int extramass = tmpmass;
+                while (extramass > 0)
+                {
+                    extramass = fuel(extramass);
+                    if (extramass > 0) _extrafuel += extramass;
+                }
+            }
+            Console.WriteLine("Day 1 Part one fuel needed: " + _fuel);
+            Console.WriteLine("Day 1 Part two fuel needed: " + (_fuel + _extrafuel));
+            Console.ReadLine();
+        }
+
+        private static int fuel(int mass)
+        {
+            mass = (int)(mass / 3) - 2;
+            return mass;
         }
 
     }
